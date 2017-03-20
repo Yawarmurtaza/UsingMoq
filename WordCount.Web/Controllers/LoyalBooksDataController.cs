032b1c8cf31e3547d38c99609ddd38067cfc47fd
Yawarmurtaza <yawarmurtaza@gmail.com>
2017-03-20 14:48:12 +0000

    page size added on the server side.

--- WordCount.Web/Controllers/LoyalBooksDataController.cs
+++ WordCount.Web/Controllers/LoyalBooksDataController.cs
@@ -19,7 +19,8 @@ namespace WordCount.Web.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<WordOccurance>> NextTenWords(string bookName = null, int numberOfRecords = 10)
+        [Route("api/LoyalBooksData/{bookName}/{pageNumber}")]
+        public async Task<IEnumerable<WordOccurance>> NextTenWords(string bookName = null, int pageNumber = 10)
         {
             if (string.IsNullOrEmpty(bookName))
             {
@@ -41,9 +42,16 @@ namespace WordCount.Web.Controllers
                 recordsToSkip += 10;
                 this.Session.SetInt32("recordsToSkip", recordsToSkip.Value);
             }
+            int total = (await this.bookManager.GetIndivisualWordsCount(bookName)).Count();
+            
+            int pageSize = 10; // set your page size, which is number of records per page
+
+            int skip = pageSize * (pageNumber - 1);
+
+            bool canPage = skip < totalItems;
             
             IEnumerable<WordOccurance> wordCount =
-                (await this.bookManager.GetIndivisualWordsCount(bookName)).Skip(recordsToSkip.Value).Take(numberOfRecords);
+                (await this.bookManager.GetIndivisualWordsCount(bookName)).Skip(skip).Take(pageSize);
             return wordCount;
         }
 
